3e5544bb8eda1c1922bfe31488e56c03a7866abe
Sanjeewa <sanjeewaxy@gmail.com>
2025-09-08 17:17:23 +0400

    mongo db connection create

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+console.log(process.env.MONGO_URI);
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server running on port 5000");
+});
