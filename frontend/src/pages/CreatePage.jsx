29076b0646a312e59c77583e3206bea1572d4a95
sanjeewa <sanjeewaxy@gmail.com>
2025-09-16 07:02:14 +0400

    product create

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,7 +1,64 @@
-const CreatePage = () => {
-    return (
-        <div>CreatePage</div>
-    )
-}
+import { Button, Container, Input, Text, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+import { useProductStore } from "../store/product";
+import { toaster } from "../components/ui/toaster";
 
-export default CreatePage
+const CreatePage = () => {
+  const [product, setProduct] = useState({
+    name: "",
+    price: "",
+    image: "",
+  });
+
+  const { createProduct } = useProductStore();
+
+  const handleSubmit = async (e) => {
+    console.log(product);
+
+    e.preventDefault();
+
+    const {success, message} = await createProduct(product);
+    console.log(success, message);
+
+    if(success){
+      toaster({
+        title: "Product created successfully",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
+  return (
+    <Container>
+      <Container maxW="container.md" bg={"gray.900"} p={6} borderRadius={6}>
+        <VStack>
+          <Text fontSize={"2xl"} fontWeight={"bold"} textAlign={"center"}>
+            Create Product
+          </Text>
+          <Input
+            onChange={(e) => setProduct({ ...product, name: e.target.value })}
+            value={product.name}
+            placeholder="Product Name"
+          />
+          <Input
+            onChange={(e) => setProduct({ ...product, price: e.target.value })}
+            value={product.price}
+            placeholder="Product Price"
+          />
+          <Input
+            onChange={(e) => setProduct({ ...product, image: e.target.value })}
+            value={product.image}
+            placeholder="Product Image"
+          />
+          <Button onClick={handleSubmit} colorScheme={"blue"} w={"full"}>
+            Create
+          </Button>
+        </VStack>
+      </Container>
+    </Container>
+  );
+};
+
+export default CreatePage;
---
+++ frontend/src/store/product.js
@@ -0,0 +1,41 @@
+import { create } from "zustand";
+import axios from "axios";
+
+export const useProductStore = create((set) => ({
+  products: [],
+  setProducts: (products) => set({ products }),
+
+  // create a function to add a product
+  createProduct: async (product) => {
+    if (!product.name || !product.price || !product.image) {
+      console.log("Please fill all the fields");
+      return {
+        status: false,
+        message: "Please fill all the fields",
+      };
+    }
+
+    const response = await axios.post(
+      "http://localhost:5000/products",
+      product,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          accept: "application/json",
+        },
+        body: JSON.stringify(product),
+      }
+    );
+    console.log(response);
+    const data = response.data;
+    console.log(data);
+    set((state) => ({
+      products: [...state.products, data],
+    }));
+
+    return {
+      status: true,
+      message: "Product created successfully",
+    };
+  },
+}));
